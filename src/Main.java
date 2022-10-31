8429c202a21bfa461b6e7c08355ccc20ab2b54a4
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2022-10-31 13:22:49 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int k;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Sınır Sayıyı Giriniz: ");
+        k = input.nextInt();
+
+        System.out.println("4'ün Kuvvetleri:");
+
+        for (int i = 1; i <= k; i *= 4) {
+            System.out.println(i);
+        }
+
+        System.out.println("5'in Kuvvetleri:");
+
+        for (int i = 1; i <= k; i *= 5) {
+            System.out.println(i);
+        }
+
+
     }
 }
